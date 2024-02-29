af588e406e0f568b4520985e233fdf1e8dca8aa6
Yoav Hainebach <yoavhainebach@gmail.com>
2024-02-29 14:47:53 +0100

    fix pagination for search bar

--- index.js
+++ index.js
@@ -10,15 +10,20 @@ const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
+const searchBar__input = document.querySelector(
+  '[data-js="search-bar__input"]'
+);
+
 // States
 let maxPage = 1;
 let page = 1;
 const searchQuery = document.querySelector('[data-js="search-bar__input"]');
+let urlExtensionString = "page=";
 
-async function fetchCharacters(urlExtensionString, urlExtention) {
+async function fetchCharacters(urlExtension) {
   try {
     const response = await fetch(
-      `https://rickandmortyapi.com/api/character/?${urlExtensionString}${urlExtention}`
+      `https://rickandmortyapi.com/api/character/?${urlExtension}`
     );
     if (!response.ok) {
       console.error("no data was fetched");
@@ -37,36 +42,44 @@ async function fetchCharacters(urlExtensionString, urlExtention) {
   }
 }
 
-// let currentPageNumber = 1;
-
 nextButton.addEventListener("click", () => {
-  while (page <= maxPage) {
+  if (page < maxPage) {
     page++;
     buttonEvent();
-    return;
   }
 });
 prevButton.addEventListener("click", () => {
-  while (page > 1) {
+  if (page > 1) {
     page--;
     buttonEvent();
-    return;
   }
 });
 
-function buttonEvent() {
+function buttonEvent(even) {
   cardContainer.innerHTML = "";
   pagination.textContent = `${page} / ${maxPage}`;
-  fetchCharacters("page=", page);
+
+  let urlExtension = "";
+
+  if (searchBar__input.value.trim() !== "") {
+    urlExtension = `name=${searchQuery.value}&page=${page}`;
+  } else {
+    urlExtension = `page=${page}`;
+  }
+
+  fetchCharacters(urlExtension);
 }
 
 searchBar.addEventListener("submit", async (event) => {
   event.preventDefault();
   cardContainer.innerHTML = "";
   maxPage = 1;
+  page = 1;
+  let urlExtension;
+  urlExtension = `name=${searchQuery.value}`;
 
   // console.log(searchQuery.value);
-  await fetchCharacters("name=", searchQuery.value);
+  await fetchCharacters(urlExtension);
 
   pagination.textContent = `${page} / ${maxPage}`;
   console.log(maxPage);
